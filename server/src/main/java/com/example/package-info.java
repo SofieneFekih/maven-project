a37518933f302c315fbc4685e57f3b78e42d2e04
Chaos R <sofienefekih07@gmail.com>
2020-01-22 11:49:54 +0100

    solve2

--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -3,17 +3,3 @@ package com.example;
 /**
  * This is a class.
  */
-public class Greeter {
-
-  /**
-   * This is a constructor.
-   */
-  public Greeter() {
-
-  }
-
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
-    return String.format("Hello, %s!", someone);
-  }
-}
