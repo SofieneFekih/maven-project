3ff19ffe7188142e6389739cc760849918087acf
Chaos R <sofienefekih07@gmail.com>
2020-01-22 14:51:29 +0100

    resolved

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,12 +11,12 @@ public class Greeter {
   public Greeter() {
 
   }
-  
+
 /** 
  *@param someone the name of person
  *@return greeting  string
  */
-  final public String greet(final String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
